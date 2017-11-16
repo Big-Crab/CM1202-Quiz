f3482a268ff70e4d6d8ddc00620d6e6e2bff83aa
JaVauban <alexjames0011@gmail.com>
2017-11-16 15:40:46 +0000

    Fixed preparation for IMGUI project

---
+++ CLion Proj/cmake-build-debug/CMakeFiles/CLion_Proj.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CLion_Proj.dir/main.cpp.obj"
+  "CLion_Proj.pdb"
+  "CLion_Proj.exe"
+  "CLion_Proj.exe.manifest"
+  "libCLion_Proj.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CLion_Proj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
